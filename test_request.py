bed057d821f842b2dfae28bd0273d862a4e59225
asianqtip <25674071+asianqtip@users.noreply.github.com>
2019-04-10 19:31:06 -0700

    Update test_request.py

--- test_request.py
+++ test_request.py
@@ -1,6 +1,6 @@
 import unittest
 import requests
 
-    def test_get_games(self):
+    def test_get_games():
         res = requests.get('http://localhost:5000/')
         self.assertEqual(res.status_code, 200)
