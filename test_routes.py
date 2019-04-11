629f0b9a260d19493f63184bbc5756fb8bde0624
Quynh <quynhhphoang@gmail.com>
2019-04-10 21:06:11 -0700

    hope this works

--- test_routes.py
+++ test_routes.py
@@ -10,6 +10,32 @@ class TestIntegrations(TestCase):
     def setUp(self):
         self.app = app.test_client()
 
-    def test_thing(self):
+    def test_home(self):
         response = self.app.get('/')
         assert response.status_code == 200
+        response = self.app.get('/home')
+        assert response.status_code == 200
+
+    def test_about(self):
+        response = self.app.get('/about')
+        assert response.status_code == 200
+
+    def test_register(self):
+        response = self.app.get('/register')
+        assert response.status_code == 200
+
+    def test_login(self):
+        response = self.app.get('/login')
+        assert response.status_code == 200
+    
+    def test_logout(self):
+        response = self.app.get('/logout')
+        assert response.status_code == 200
+
+    def test_results(self):
+        response = self.app.get('/results')
+        assert response.status_code == 200
+
+    if __name__ == "__main__":
+    unittest.main()
+        
