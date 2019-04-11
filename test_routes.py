eb47c8f55a58490b61d3804e79573704daa4878d
Quynh <quynhhphoang@gmail.com>
2019-04-10 17:45:07 -0700

    maybe now

--- test_routes.py
+++ test_routes.py
@@ -1,5 +1,27 @@
 from flask import request
+from flask import Flask
 from flaskblog import routes
+
+def test_home():
+    app=Flask(__name__)
+    client = app.test_client()
+    url='l'
+    response = client.get(url)
+    assert response.status_code == 200
+
+def test_home():
+    app=Flask(__name__)
+    client = app.test_client()
+    url='l'
+    response = client.get(url)
+    assert response.status_code == 200
+
+def test_home():
+    app=Flask(__name__)
+    client = app.test_client()
+    url='l'
+    response = client.get(url)
+    assert response.status_code == 200
 
 def test_home():
     app=Flask(__name__)
