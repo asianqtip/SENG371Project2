4201bca71b82bacf601e14cc6222d37d15b672a1
asianqtip <quynhhphoang@gmail.com>
2019-04-10 19:54:48 -0700

    testing

---
+++ test_routes.py
@@ -0,0 +1,9 @@
+from flask import requests
+from flaskblog import routes
+
+def test_home()
+    app=Flask(__name__)
+    client = app.test_client()
+    url='l'
+    response = client.get(url)
+    assert response.status_code == 200
